7a00384d38a869b0ecb26f109721c8e680248c87
Maryna <maryna.pylypchenko@gmail.com>
2020-11-15 15:35:31 +0200

    commit

---
+++ src/flight.actions.js
@@ -0,0 +1,19 @@
+import { fetchFlights } from "./flight.gateway";
+
+export const FLIGHTS_LIST = "AIRPORT/FLIGHTS_LIST";
+
+export const flightsList = (dataList, date) => {
+    return {
+        type: FLIGHTS_LIST,
+        payload: { dataList, date },
+    };
+};
+
+export const fetchFlightsList = (date) => {
+    const thunkAction = (dispatch) => {
+        fetchFlights(date).then((dataList) => {
+            dispatch(flightsList(dataList, date));
+        });
+    };
+    return thunkAction;
+};
